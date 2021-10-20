8f5f20ac766e65ab8cee29e7611c8dd2686cc314
Konstantin Seiler <list@kseiler.de>
2021-10-20 14:46:00 +1100

    Improve error handling when opening a cog fails.

--- terracotta/cog.py
+++ terracotta/cog.py
@@ -39,120 +39,126 @@ def check_raster_file(src_path: str) -> ValidationInfo:  # pragma: no cover
 
     config = dict(GDAL_DISABLE_READDIR_ON_OPEN='FALSE')
     with rasterio.Env(**config):
-        with rasterio.open(src_path) as src:
-            if not src.driver == 'GTiff':
-                errors.append('The file is not a GeoTIFF')
-                return errors, warnings, details
+        try:
+            src = rasterio.open(src_path)
+        except rasterio.errors.RasterioIOError as e:
+            errors.append(str(e))
+            return errors, warnings, details
+        else:
+            with rasterio.open(src_path) as src:
+                if not src.driver == 'GTiff':
+                    errors.append('The file is not a GeoTIFF')
+                    return errors, warnings, details
 
-            filelist = [os.path.basename(f) for f in src.files]
-            src_bname = os.path.basename(src_path)
-            if len(filelist) > 1 and src_bname + '.ovr' in filelist:
-                errors.append(
-                    'Overviews found in external .ovr file. They should be internal'
-                )
-
-            overviews = src.overviews(1)
-            if src.width >= 512 or src.height >= 512:
-                if not src.is_tiled:
+                filelist = [os.path.basename(f) for f in src.files]
+                src_bname = os.path.basename(src_path)
+                if len(filelist) > 1 and src_bname + '.ovr' in filelist:
                     errors.append(
-                        'The file is greater than 512xH or 512xW, but is not tiled'
+                        'Overviews found in external .ovr file. They should be internal'
                     )
 
-                if not overviews:
-                    warnings.append(
-                        'The file is greater than 512xH or 512xW, it is recommended '
-                        'to include internal overviews'
-                    )
-
-            ifd_offset = int(src.get_tag_item('IFD_OFFSET', 'TIFF', bidx=1))
-            ifd_offsets = [ifd_offset]
-            if ifd_offset > 300:
-                errors.append(
-                    f'The offset of the main IFD should be < 300. It is {ifd_offset} instead'
-                )
-
-            details['ifd_offsets'] = {}
-            details['ifd_offsets']['main'] = ifd_offset
-
-            if not overviews == sorted(overviews):
-                errors.append('Overviews should be sorted')
-
-            for ix, dec in enumerate(overviews):
-
-                # NOTE: Size check is handled in rasterio `src.overviews` methods
-                # https://github.com/mapbox/rasterio/blob/4ebdaa08cdcc65b141ed3fe95cf8bbdd9117bc0b/rasterio/_base.pyx
-                # We just need to make sure the decimation level is > 1
-                if not dec > 1:
-                    errors.append(
-                        'Invalid Decimation {} for overview level {}'.format(dec, ix)
-                    )
-
-                # Check that the IFD of descending overviews are sorted by increasing
-                # offsets
-                ifd_offset = int(src.get_tag_item('IFD_OFFSET', 'TIFF', bidx=1, ovr=ix))
-                ifd_offsets.append(ifd_offset)
-
-                details['ifd_offsets']['overview_{}'.format(ix)] = ifd_offset
-                if ifd_offsets[-1] < ifd_offsets[-2]:
-                    if ix == 0:
+                overviews = src.overviews(1)
+                if src.width >= 512 or src.height >= 512:
+                    if not src.is_tiled:
                         errors.append(
-                            'The offset of the IFD for overview of index {} is {}, '
-                            'whereas it should be greater than the one of the main '
-                            'image, which is at byte {}'.format(
-                                ix, ifd_offsets[-1], ifd_offsets[-2]
+                            'The file is greater than 512xH or 512xW, but is not tiled'
+                        )
+
+                    if not overviews:
+                        warnings.append(
+                            'The file is greater than 512xH or 512xW, it is recommended '
+                            'to include internal overviews'
+                        )
+
+                ifd_offset = int(src.get_tag_item('IFD_OFFSET', 'TIFF', bidx=1))
+                ifd_offsets = [ifd_offset]
+                if ifd_offset > 300:
+                    errors.append(
+                        f'The offset of the main IFD should be < 300. It is {ifd_offset} instead'
+                    )
+
+                details['ifd_offsets'] = {}
+                details['ifd_offsets']['main'] = ifd_offset
+
+                if not overviews == sorted(overviews):
+                    errors.append('Overviews should be sorted')
+
+                for ix, dec in enumerate(overviews):
+
+                    # NOTE: Size check is handled in rasterio `src.overviews` methods
+                    # https://github.com/mapbox/rasterio/blob/4ebdaa08cdcc65b141ed3fe95cf8bbdd9117bc0b/rasterio/_base.pyx
+                    # We just need to make sure the decimation level is > 1
+                    if not dec > 1:
+                        errors.append(
+                            'Invalid Decimation {} for overview level {}'.format(dec, ix)
+                        )
+
+                    # Check that the IFD of descending overviews are sorted by increasing
+                    # offsets
+                    ifd_offset = int(src.get_tag_item('IFD_OFFSET', 'TIFF', bidx=1, ovr=ix))
+                    ifd_offsets.append(ifd_offset)
+
+                    details['ifd_offsets']['overview_{}'.format(ix)] = ifd_offset
+                    if ifd_offsets[-1] < ifd_offsets[-2]:
+                        if ix == 0:
+                            errors.append(
+                                'The offset of the IFD for overview of index {} is {}, '
+                                'whereas it should be greater than the one of the main '
+                                'image, which is at byte {}'.format(
+                                    ix, ifd_offsets[-1], ifd_offsets[-2]
+                                )
                             )
+                        else:
+                            errors.append(
+                                'The offset of the IFD for overview of index {} is {}, '
+                                'whereas it should be greater than the one of index {}, '
+                                'which is at byte {}'.format(
+                                    ix, ifd_offsets[-1], ix - 1, ifd_offsets[-2]
+                                )
+                            )
+
+                block_offset = int(src.get_tag_item('BLOCK_OFFSET_0_0', 'TIFF', bidx=1))
+                if not block_offset:
+                    errors.append('Missing BLOCK_OFFSET_0_0')
+
+                data_offset = int(block_offset) if block_offset else 0
+                data_offsets = [data_offset]
+                details['data_offsets'] = {}
+                details['data_offsets']['main'] = data_offset
+
+                for ix, dec in enumerate(overviews):
+                    data_offset = int(
+                        src.get_tag_item('BLOCK_OFFSET_0_0', 'TIFF', bidx=1, ovr=ix)
+                    )
+                    data_offsets.append(data_offset)
+                    details['data_offsets']['overview_{}'.format(ix)] = data_offset
+
+                if data_offsets[-1] < ifd_offsets[-1]:
+                    if len(overviews) > 0:
+                        errors.append(
+                            'The offset of the first block of the smallest overview '
+                            'should be after its IFD'
                         )
                     else:
                         errors.append(
-                            'The offset of the IFD for overview of index {} is {}, '
-                            'whereas it should be greater than the one of index {}, '
-                            'which is at byte {}'.format(
-                                ix, ifd_offsets[-1], ix - 1, ifd_offsets[-2]
-                            )
+                            'The offset of the first block of the image should '
+                            'be after its IFD'
                         )
 
-            block_offset = int(src.get_tag_item('BLOCK_OFFSET_0_0', 'TIFF', bidx=1))
-            if not block_offset:
-                errors.append('Missing BLOCK_OFFSET_0_0')
+                for i in range(len(data_offsets) - 2, 0, -1):
+                    if data_offsets[i] < data_offsets[i + 1]:
+                        errors.append(
+                            'The offset of the first block of overview of index {} should '
+                            'be after the one of the overview of index {}'.format(i - 1, i)
+                        )
 
-            data_offset = int(block_offset) if block_offset else 0
-            data_offsets = [data_offset]
-            details['data_offsets'] = {}
-            details['data_offsets']['main'] = data_offset
-
-            for ix, dec in enumerate(overviews):
-                data_offset = int(
-                    src.get_tag_item('BLOCK_OFFSET_0_0', 'TIFF', bidx=1, ovr=ix)
-                )
-                data_offsets.append(data_offset)
-                details['data_offsets']['overview_{}'.format(ix)] = data_offset
-
-            if data_offsets[-1] < ifd_offsets[-1]:
-                if len(overviews) > 0:
+                if len(data_offsets) >= 2 and data_offsets[0] < data_offsets[1]:
                     errors.append(
-                        'The offset of the first block of the smallest overview '
-                        'should be after its IFD'
+                        'The offset of the first block of the main resolution image '
+                        'should be after the one of the overview of index {}'.format(
+                            len(overviews) - 1
+                        )
                     )
-                else:
-                    errors.append(
-                        'The offset of the first block of the image should '
-                        'be after its IFD'
-                    )
-
-            for i in range(len(data_offsets) - 2, 0, -1):
-                if data_offsets[i] < data_offsets[i + 1]:
-                    errors.append(
-                        'The offset of the first block of overview of index {} should '
-                        'be after the one of the overview of index {}'.format(i - 1, i)
-                    )
-
-            if len(data_offsets) >= 2 and data_offsets[0] < data_offsets[1]:
-                errors.append(
-                    'The offset of the first block of the main resolution image '
-                    'should be after the one of the overview of index {}'.format(
-                        len(overviews) - 1
-                    )
-                )
 
         for ix, dec in enumerate(overviews):
             with rasterio.open(src_path, OVERVIEW_LEVEL=ix) as ovr_dst:
